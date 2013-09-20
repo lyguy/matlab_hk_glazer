d7c2f0bf374bcf0f7f365736deec8b88fbd6237e
Lyman Gillispie <lyman.gillispie@gmail.com>
2013-09-20 11:38:58 -0800

    add comments

--- +glazer/@EntryGetter/EntryGetter.m
+++ +glazer/@EntryGetter/EntryGetter.m
@@ -1,10 +1,17 @@
 classdef EntryGetter
+%  EntryGetter
+%
+%  Object to take in a string, delimited by newlines, and allow
+%  one to look up words in the string based upon line number
+%  and position in the line.
+%
   properties
-    lines
+    lines % array of lines taken from originating string.
   end %properties
     
   methods
     function eg = EntryGetter(str)
+      % constrctor function
       %Construct a cell array from the lines/words in the string
       
       l = strread(str, '%s', 'delimiter','\n');
@@ -15,6 +22,8 @@ classdef EntryGetter
     
     function s = linePos(self, line, word)
       %Return the line,pos word in the initialization string
+      % e.g. linePos(5, 2) returns the second word in the fifth
+      % line of str.
       s = self.lines{line}{word};
     end
   end %methods
